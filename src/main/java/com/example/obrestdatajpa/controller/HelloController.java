af57de1f78b6a763e921b487b465878394d6587a
agaray <angaray93@gmail.com>
2022-07-01 09:23:46 -0400

    spring heroku app config

--- src/main/java/com/example/obrestdatajpa/controller/HelloController.java
+++ src/main/java/com/example/obrestdatajpa/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.obrestdatajpa.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,7 @@ public class HelloController {
     @Value("${app.varexample}")
     String message;
 
+    @GetMapping("/hola")
     public String holaMundo() {
         System.out.println(message);
         return "Hola mundo";
